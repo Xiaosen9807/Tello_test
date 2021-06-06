047e04d1c27b9d848a0afdc0d7f64d4a1d248f7c
Qinxusen <184685245@qq.com>
2021-06-06 23:30:49 +0800

    https://github.com/Xiaosen9807/Tello_test.git

--- tello/image.py
+++ tello/image.py
@@ -1,6 +1,6 @@
 from djitellopy import tello
 import cv2
-from keypress import getKey
+import pygame
 
 
 me = tello.Tello()
@@ -9,8 +9,27 @@ print(me.get_battery()) #The battery of the drone
 
 me.streamon() #open stream
 
+def init():
+    pygame.init()
+    win = pygame.display.set_mode((400,400))
+
+def getKey(keyName):
+    ans = False 
+    keyInput = pygame.key.get_pressed()
+    myKey = getattr(pygame,'K_{}'.format(keyName))
+    if keyInput[myKey]:
+        ans = True
+    pygame.display.update()
+    return ans
+
+def getKeyboardInput():
+
+    if getKey("x"): me.streamoff()
+    elif getKey("c"): me.streamon()
 
 while True:
+    Values=getKeyboardInput()
+    me.send_rc_control(Values)
     img = me.get_frame_read().frame
     img = cv2.resize(img, (360,240)) #reduce resolution to speed up the transmission of stream
     cv2.imshow("Image",img) #creat a window to show the stream signal
